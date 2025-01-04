071b6ef669bce2fd2cd202b64788e5f5ff1f8c65
a-hakki <abdelazizhatake@gmail.com>
2025-01-04 15:34:56 +0100

    put_image

---
+++ fdf_image/fdf.h
@@ -0,0 +1,45 @@
+#ifndef FDF_H
+# define FDF_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include "../minilibx-linux/mlx.h"
+#include "../ft_libft/libft.h"
+#include "../ft_printf/ft_printf.h"
+#include "../Get_next_line/get_next_line.h"
+#include <math.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#define M_PI 3.14159265358979323846
+#define M_WIDTH 1920
+#define M_HEIGHT 1080
+
+typedef struct s_coordinates
+{
+    int X0;
+    int Y0;
+    int X1;
+    int Y1;
+    int **color;
+    int c;
+    int **tab;
+    int lines;
+    int columns;
+}               t_vec;
+
+
+typedef struct	s_vars
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+    char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+    float     x;
+}				t_vars;
+
+int     ft_color(const char *s);
+
+#endif
